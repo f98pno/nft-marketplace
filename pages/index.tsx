57370dddcc84f410651a8ad499219d4d1e48cb56
Peter Novak <peter.novak.m@gmail.com>
2022-07-30 16:12:32 +0800

    Adding web3 provider and states

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,28 @@ import type { NextPage } from 'next'
 import { BaseLayout, NftList } from '@ui'
 import nfts from "../content/meta.json"
 import { NftMeta } from '@_types/nft'
+import { useWeb3 } from '@providers/web3'
 
 const Home: NextPage = () => {
+  const { ethereum, provider, isLoading, contract } = useWeb3();
+  console.log(ethereum)
+  console.log(provider)
+
+  const getAccounts = async () => {
+    const accounts = await provider!.listAccounts()
+    console.log(accounts[0])
+  }
+
+  if (provider) {
+    getAccounts();
+  }
+
   return (
     <BaseLayout>
+      {`Loading: ${isLoading}, `}
+      {`Ethereum: ${ethereum}, `}
+      {`Provider: ${provider}, `}
+      {`Contract: ${contract}, `}
       <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
         <div className="absolute inset-0">
           <div className="bg-white h-1/3 sm:h-2/3" />
